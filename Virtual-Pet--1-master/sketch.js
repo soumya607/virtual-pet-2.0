be078198463f067bedfaacd6b688bf298caa7561
soumya607 <65755872+soumya607@users.noreply.github.com>
2021-03-10 22:23:47 +0530

    Add files via upload

---
+++ Virtual-Pet--1-master/sketch.js
@@ -0,0 +1,65 @@
+//Create variables here
+var dogIMG,happyDogIMG;
+var foodS,foodstock,database;
+
+function preload()
+{
+  //load images here
+  dogIMG=loadImage("images/dogImg.png")
+  happydogIMG=loadImage("images/dogImg1.png")
+  
+}
+
+function setup() {
+	createCanvas(500, 500);
+  rectMode(CENTER);
+  database = firebase.database();
+
+  dog = createSprite(width/2, 270, 10,10);
+	dog.addImage(dogIMG)
+  dog.scale=0.2
+  
+  foodstock = database.ref('food');
+  foodstock.on("value",readStock );
+
+}
+
+
+function draw() {  
+  background(46,139,87)
+  
+  if(keyWentDown(UP_ARROW)){
+     writeStock(foodS);
+    dog.addImage(happydogIMG);
+    dog.scale = 0.2
+
+
+  }
+  drawSprites();
+  
+  //add styles here
+  textSize(15);
+  fill("black");
+  stroke("black")
+  text("Note: Press UP_ARROW key to Feed dog Milk!!", 90 ,60)
+  text("Food Left : " + foodS , 185,25)
+}
+
+function readStock(data){
+  foodS = data.val();
+  database.ref('/').on({
+    data
+  })
+}
+
+//function to write values in DB
+function writeStock(x){
+   if(x <= 0){
+     x=0;
+   }else{
+     x=x-1;
+   }
+  database.ref('/').update({
+    food:x
+  })
+}
